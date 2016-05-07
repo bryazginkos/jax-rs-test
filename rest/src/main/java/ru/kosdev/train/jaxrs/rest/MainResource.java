a6ea07b5f5852d767aa415128061180357d243c9
kos <bryazginkos@yahoo.com>
2016-05-07 11:09:31 +0600

    spring4

--- rest/src/main/java/ru/kosdev/train/jaxrs/rest/MainResource.java
+++ rest/src/main/java/ru/kosdev/train/jaxrs/rest/MainResource.java
@@ -1,5 +1,8 @@
 package ru.kosdev.train.jaxrs.rest;
 
+import org.springframework.stereotype.Controller;
+
+import javax.annotation.PostConstruct;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
@@ -7,6 +10,7 @@ import javax.ws.rs.Path;
  * Created by kbryazgin on 5/6/2016.
  */
 @Path("/")
+@Controller
 public class MainResource  {
 
     @GET
